40370909d225e966e8bb9e41a8854226f5ce33a1
zuo_ji <zuo_ji@worksap.co.jp>
2020-06-12 17:40:43 +0800

    Add test

--- test/func.test.ts
+++ test/func.test.ts
@@ -1,21 +1,11 @@
 import test from 'ava';
 
-function getArgs(func, args) {
-    var STRIP_COMMENTS = /(\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s*=[^,\)]*(('(?:\\'|[^'\r\n])*')|("(?:\\"|[^"\r\n])*"))|(\s*=[^,\)]*))/mg;
-    var ARGUMENT_NAMES = /([^\s,]+)/g;
-    var fnStr = func.toString().replace(STRIP_COMMENTS, '');
-    var result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-    if (result === null)
-        result = [];
-    return result;
-}
-
-
+import { getParamNames } from '../src/public-api'
 
 
 
 test('template', async t => {
-    let list = getArgs(function (name: string, id: number) { }, [])
+    let list = getParamNames(function (name: string, id: number) { })
     console.log(list)
     t.deepEqual(list, ['name', 'id'])
 })
